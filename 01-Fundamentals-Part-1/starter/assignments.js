94e836846d9ba90e49a4857e6ab520c8d0c8409c
Sander Brustad <sander.simonsen.brustad@google.com>
2022-03-31 22:06:36 +0200

    Add coding challenge 2

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -43,3 +43,15 @@ console.log(bmiJohnTwo);
 console.log(markHigherBmiOne);
 console.log(markHigherBmiTwo);
 */
+
+// Coding challenge 2
+/*
+const markBMI = 28.3;
+const johnBMI = 23.9;
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})`);
+} else {
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})`);
+}
+*/
