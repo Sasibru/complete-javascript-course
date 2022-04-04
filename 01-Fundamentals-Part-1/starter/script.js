e332c0c650c254316029a3bbd520da47136925da
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-04 14:14:13 +0200

    Add boolean values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -67,3 +67,27 @@ console.log(century);
 // console.log('23' - '10' - 3); // Minus converter strings til numbers
 // console.log('23' * '2'); //Converter strings til numbers
 // console.log('23' / '2'); //Converter strings til numbers
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+//console.log(Boolean(0)); //false
+//console.log(Boolean(undefined)); //false
+//console.log(Boolean('Sander')); //true
+//console.log(Boolean({})); //true
+//console.log(Boolean('')); //false
+/*
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)")
+} else {
+    console.log("You should get a job!")
+}
+
+let height;
+if (height >= 0) {
+    console.log('Yay');
+} else {
+    console.log('Height is UNDEFINED')
+}
+*/
+
