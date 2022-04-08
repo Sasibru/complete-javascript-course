209645aa4b4739fb3949d8f7930feebbbdb9f9e2
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-08 13:28:55 +0200

    Add completed switch statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -152,3 +152,38 @@ if (favourite === 23) { // '23' == 23
 // } else {
 //     console.log('Someone else should drive..')
 // }
+
+// The switch statement
+
+// const day = 'monday';
+
+// switch (day) {
+//     case 'monday': // day === 'monday'
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend :)');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+// }
+// Same code in if else statements
+// if (day === 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//     console.log('Prepare theory videos');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//     console.log('Write code examples');
+// }
