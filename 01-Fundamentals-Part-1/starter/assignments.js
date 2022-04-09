3f821e23d3f85afcb2d73cf341f24aeff1ac2bfb
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-09 19:17:35 +0200

    Add coding challenge 4

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -85,3 +85,10 @@ if (dolphins > koalas && dolphins >= 100) {
 }
 */
 
+
+// Coding Challenge #4
+
+// const billValue = 275;
+// const tip = billValue <= 300 && billValue >= 50 ? billValue * 0.15 : billValue * 0.2;
+
+// console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue + tip}`);
