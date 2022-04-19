77ff3ba60d24ff8c44d6258f932c29bffb56975b
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-19 14:58:36 +0200

    Add strict mode section

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :)')
+
+// const interface = 'Audio';
+// const private = 236;
+// const if = 23;
+
+// FUNCTIONS
