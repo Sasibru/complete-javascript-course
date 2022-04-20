ff3661fa6d324c8328969a146ee533a55f72ea79
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-20 13:54:25 +0200

    Add functions WIP

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,13 +1,49 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive :)')
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :)')
 
 // const interface = 'Audio';
 // const private = 236;
 // const if = 23;
 
 // FUNCTIONS
+
+function logger() {
+    console.log('My name is Sander');
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
+}
+
+const age1 = calcAge1(1995);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2022 - birthYear;
+}
+const age2 = calcAge2(1995);
+
+console.log(age1, age2);
