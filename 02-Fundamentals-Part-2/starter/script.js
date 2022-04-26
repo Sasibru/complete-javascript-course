5892a9fb4486f0327af8268505e33d59236c93d2
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-26 13:21:10 +0200

    Add arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,38 +12,49 @@
 
 // FUNCTIONS
 
-function logger() {
-    console.log('My name is Sander');
-}
+// function logger() {
+//     console.log('My name is Sander');
+// }
 
 // calling / running / invoking function
-logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-    return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
 
 
 // Function declaration
-function calcAge1(birthYear) {
-    return 2022 - birthYear;
-}
+// function calcAge1(birthYear) {
+//     return 2022 - birthYear;
+// }
 
-const age1 = calcAge1(1995);
-console.log(age1);
+// const age1 = calcAge1(1995);
+// console.log(age1);
 
 // Function expression
-const calcAge2 = function (birthYear) {
-    return 2022 - birthYear;
-}
-const age2 = calcAge2(1995);
+// const calcAge2 = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+// const age2 = calcAge2(1995);
 
-console.log(age1, age2);
+// Arrow function
+// const calcAge3 = birthYear => 2022 - birthYear;
+// const age3 = calcAge3(1995);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2022 - birthYear;
+//     const retirement = 65 - age;
+    // return retirement;
+//     return `${firstName} retires in ${retirement} years`
+// }
+
+// console.log(yearsUntilRetirement(1995, 'Sander'));
