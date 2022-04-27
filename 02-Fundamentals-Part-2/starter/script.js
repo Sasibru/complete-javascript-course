34292e7e2f5d082fd2186f18df44350ba46c1197
Sander Brustad <sander.simonsen.brustad@google.com>
2022-04-27 12:39:35 +0200

    Add functions review

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -79,3 +79,26 @@
 
 // console.log(fruitProcessor(2, 3));
 
+
+// const calcAge = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(`${firstName} retires in ${retirement} years`)
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has already retired!`)
+//         return -1;
+//     }
+
+    // return retirement;
+
+// }
+// console.log(yearsUntilRetirement(1995, 'Sander'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
