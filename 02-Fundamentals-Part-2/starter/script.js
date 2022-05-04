565d5d2a2b694f05b083ad25cf85746be5fa4925
Sander Brustad <sander.simonsen.brustad@google.com>
2022-05-04 13:50:23 +0200

    Add while loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -330,6 +330,7 @@ for (let i = 0; i < sanderArray.length; i++) {
 }
 */
 
+/*
 const sanderArray = [
     'Sander',
     'Brustad',
@@ -338,3 +339,40 @@ const sanderArray = [
     ['Oscar', 'Andreas', 'Joakim']
 ];
 
+// Loop inside a loop
+for (let i = sanderArray.length - 1; i >= 0; i--) {
+    console.log(i, sanderArray[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`----------- Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repitition ${rep}`);
+    }
+}
+*/
+
+// While LOOP
+/*
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weights repetition ${rep}`);
+// }
+
+
+let rep = 1;
+while (rep <= 10) {
+    // console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice)
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    
+    if (dice === 6) console.log(`Congrats, you rolled a ${dice}!`)
+}
+*/
