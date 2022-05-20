7b86b5192153957c4e3876b61f9f7a1327ce9a1d
Sander Brustad <sander.simonsen.brustad@google.com>
2022-05-20 15:52:50 +0200

    Add scoping code

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -2,9 +2,31 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      // Creating new variable with same name as outer scope variable
+      const firstName = 'Steven';
+      //Reassigning outer scope variable
+      output = 'New output!';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      // function add(a, b) {
+      //   return a + b;
+      // }
+    }
+
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
   return age;
 }
 
 const firstName = 'Sander';
-calcAge(1991);
+calcAge(1995);
